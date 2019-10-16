7289a6c842c3116955c5b6a741d1cfc51dc5add4
wesbarron <barron_wesley@columbusstate.edu>
2019-10-16 01:59:45 +0000

    remove task

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.post('/addtask', function(req, res){
 
 app.post('/removetask', function(req, res){
     var deleteTask = req.body.newtask;
-    task.pop(deleteTask);
+    task.delete(deleteTask);
     removedTask.push(deleteTask);
     res.redirect('/');
 })
