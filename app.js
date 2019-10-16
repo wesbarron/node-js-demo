3a9f90fbf5df0b2edc4cc0920bc42ac49689a207
wesbarron <barron_wesley@columbusstate.edu>
2019-10-16 14:31:52 +0000

    trying to remove list item

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.post('/removetask', function(req, res){
     var deleteTask = req.body.newtask;
 
     //if(task.checked == true){
-    task.splice(task.checked == true);
+    console.log(req.body.__dirname);
     //}
     //task.push(res);
     //removedTask.push(deleteTask);
