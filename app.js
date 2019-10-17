812a6402f1161d3360ac489ad972ec931aeccd5c
wesbarron <barron_wesley@columbusstate.edu>
2019-10-17 13:33:38 +0000

    changes to app and index

--- app.js
+++ app.js
@@ -23,9 +23,14 @@ app.post('/addtask', function(req, res){
 });
 
 app.post('/removetask', function(req, res){
-    var deleteTask = req.param.id;
+    var deleteTask = req.param.id.checked;
 
-    task.splice(task.filter(req.value.checked));
+    if(deleteTask){
+    task.splice(req.params.id);
+    }
+    else{
+        res.send("no deletion of " + req.params.id);
+    }
 
     res.redirect('/');
 })
