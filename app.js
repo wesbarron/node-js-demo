dae131e48b5dbd9d9fefe7dbc2f6791805108026
wesbarron <barron_wesley@columbusstate.edu>
2019-10-17 18:08:19 +0000

    broken

--- app.js
+++ app.js
@@ -25,10 +25,10 @@ app.post('/addtask', function(req, res){
 app.post('/removetask', function(req, res){
    //var deleteTask = req.params.id;
 
-    
 
 
-        removedTask.push("other");
+
+        //removedTask.push("other");
 
     res.redirect('/');
 });
