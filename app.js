e01d2bc55b9697547549105157e4d23a887710a0
wesbarron <barron_wesley@columbusstate.edu>
2019-10-22 22:44:09 +0000

    added mongodb

--- app.js
+++ app.js
@@ -2,33 +2,30 @@ var http = require('http');
 var path = require('path');
 var express = require("express");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", 'ejs');
-app.use(bodyParser.urlencoded({ encoded: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ encoded: false}));
 app.use(express.static("public"));
+const Todo = require('./model/todo.model');
+const mongoDB = 'mongodb+srv://mongoTest:1234@cluster0-5iztt.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDB connection error:"))
 
 var task = [];
 var complete = [];
-/*
+
 app.get('/', function(req, res){
     res.render("index", {task:task, complete:complete});
 });
-*/
-//var url = "http://xkcd.com/614/info.0.json";
-//var newComic = fetch(url);
-/*
-    .then((resp) => resp.json())
-    .then(function(data){
-        var newComic = data.results;
-    });*/
 
-app.get('/', function(req, res){
-    res.render("comic", {});
-});
-/*
+
 app.post('/addtask', function(req, res){
     var newTask = req.body.newtask;
     if (newTask !== ""){
@@ -52,19 +49,7 @@ app.post('/removetask', function(req, res){
     }
     res.redirect('/');
 });
-*/
 
-//random comic post
-
-app.post('/addComic', function(req, res){
-
-    //var comicYear = newComic.year;
-    //var comicImg = newComic.img;
-
-    //res.send(newComic);
-
-   //res.redirect('/');
-});
 
 http.createServer(app).listen(port, function(){
 
