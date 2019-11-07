0fe8b49621635182f3cce018f430090c8fcbe28d
wesbarron <barron_wesley@columbusstate.edu>
2019-11-07 04:27:56 +0000

    making repairs

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+
+    //write the function to do fetch
+
+    render(){
+        return (
+            <h1>This is a dynamic web page via node and react</h1>
+        );
+    }
+}
+
+export default Home;
